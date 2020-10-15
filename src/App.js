877ebc45267b58857eab7372f3970cf87edf8163
Aaron de Boer <aarondeboerwk@gmail.com>
2020-10-16 01:21:25 +0200

    adding shoping page with collection-item component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route } from 'react-router-dom';
+import ShopPage from './pages/shoppage/shoppage.component';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div>
     	<Switch>
     		<Route exact path='/' component={HomePage}/>
+    		<Route exact path='/shop' component={ShopPage}/>
     	</Switch>
     </div>
   );
