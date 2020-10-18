30c39c2cfc6cb0e05610b191f118dc79c7864372
Aaron de Boer <aarondeboerwk@gmail.com>
2020-10-18 17:21:13 +0200

    added sign up component and made the sign in page more compleet with the sign up functionality. Users wil be stored in the database on Firestore from Firebase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from './pages/shoppage/shoppage.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-sign-uppage/sign-in-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import './App.css';
 
 class App extends React.Component {
@@ -19,8 +19,21 @@ class App extends React.Component {
     unsubscribeFromAuth = null;
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-            this.setState({currentUser: user});
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+            if (userAuth){
+                const userRef = await createUserProfileDocument(userAuth);
+
+                userRef.onSnapshot(snapShot => {
+                    this.setState({
+                        currentUser: {
+                            id: snapShot.id,
+                            ...snapShot.data()
+                        }
+                    })
+                });
+            } else {
+                this.setState({currentUser: userAuth});
+            }
         });
     }
 
