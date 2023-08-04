1b140aae0d09b5c3b61d68083f9e25d470679ef4
Aaron de Boer <aarondeboerwk@gmail.com>
2023-08-04 16:01:20 +0200

    Added persisted store and enabled redux dev tools extension

--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,40 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
 
+// Configuration for redux persist
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+// Create persisted reducer
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // All the middlewares that will run before actions hit the reducers
-const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(
+  Boolean
+);
+
+// Allow redux devtools extension in chrome to work if installed
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
 
 // Activate the middlewares
-const composedEnhancers = compose(applyMiddleware(...middleWares));
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
